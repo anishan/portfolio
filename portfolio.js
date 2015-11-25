7b04d37f121d608620172a8ac81bc9bf938269e2
anishan <anisha.nakagawa@students.olin.edu>
2015-11-25 14:17:36 -0500

    pulling projects from a csv file

--- portfolio.js
+++ portfolio.js
@@ -7,7 +7,12 @@ function run()
 
 function update_projects()
 {
-  readCSV(function(data){});
+  readCSV(function(data)
+  {
+    fillPortfolio(data)
+  });
+
+
 }
 
 function readCSV(f)
@@ -22,6 +27,33 @@ function readCSV(f)
       {
         // add the new data to GLOBAL.data
          GLOBAL.projects.push.apply(GLOBAL.projects, data)
+         f(data)
       }
     });
+}
+
+function fillPortfolio(data)
+{
+  // Define which div (from the html file) we will be using
+  var projectElement = document.getElementById("project_list");
+
+  for (var i = 0; i < GLOBAL.projects.length; i++)
+  {
+    // Create div to hold all info for each project
+    var projectItem = document.createElement("div")
+    projectElement.appendChild(projectItem)
+    projectItem.className = "project_item"
+
+    // Create and add the title to the div, as a H2 level
+    var title = document.createElement("H2")
+    projectItem.appendChild(title)
+    var titleText = document.createTextNode(GLOBAL.projects[i]["Title"])
+    title.appendChild(titleText);
+
+    // Create and add the text to the div
+    var title = document.createElement("p")
+    projectItem.appendChild(title)
+    var titleText = document.createTextNode(GLOBAL.projects[i]["Description"])
+    title.appendChild(titleText);
+  }
 }
